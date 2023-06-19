e524b5a20db90fa0dbf43b07df3403fd26978420
Chintha Murali Krishna <chintha.muralikrishna@hudini.io>
2023-06-19 10:28:55 +0530

    create degree issue
    slove the issue in degree

--- FinancialApp/ViewController.swift
+++ FinancialApp/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("welcome")
         // Do any additional setup after loading the view.
     }
-
-
+  
+        
+    
 }
 
